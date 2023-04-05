c8554a6d9df6e4a349b50b1833c47fbf0acb048f
schnoddelbotz <jan@hacker.ch>
2023-04-05 03:46:11 +0200

    make TLD2Country work

--- internationalization/iso3166/main.go
+++ internationalization/iso3166/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -49,18 +51,16 @@ func main() {
 }
 
 func produceGo(data []iso3166) {
-	fmt.Printf("type var CountryFlags = map[string]string{ ...\n")
+	fmt.Printf("type var TLD2Country = map[string]string{\n")
 	for _, country := range data {
-		fmt.Printf("c:%s -> tld:%s\n", country.CountryName, country.InternetccTLD)
+		fmt.Printf(`  "%s": "%s",`+"\n", country.InternetccTLD, country.CountryName)
 	}
-	fmt.Printf("} todo ... and all variants we need.")
+	fmt.Printf("}\n # todo ... and all other variants we need.")
 	// dumpTableToStringMap(1,3) // i.e. provide column numbers, construct map name based on column name?
 	// dumpTableToMap(1,[2,4,5,6]) // i.e. create a struct with "correct" automatic names
 }
 
 func readhtml(file string) []iso3166 {
-	result := []iso3166{}
-
 	htmlFile, err := os.Open(file)
 	fatal(err)
 	defer htmlFile.Close()
@@ -70,9 +70,8 @@ func readhtml(file string) []iso3166 {
 	var tableNode *html.Node
 	var fn func(*html.Node)
 	fn = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "table" {
-			log.Printf("Found table. Attributes: %+v", n.Attr)
-			log.Print(renderNode(n))
+		if n.Type == html.ElementNode && n.Data == "table" &&
+			strings.Contains(getElementAttributeValue(n.Attr, "class"), "wikitable") {
 			tableNode = n
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
@@ -80,11 +79,68 @@ func readhtml(file string) []iso3166 {
 		}
 	}
 	fn(htmldoc)
+	if tableNode == nil {
+		fatal(errors.New("no table of CSS class wikitable found in HTML; tried hard, very"))
+	}
+	return readtable(tableNode)
+}
 
-	log.Printf("F node = %+v", tableNode)
+func readtable(table *html.Node) []iso3166 {
+	result := []iso3166{}
+	log.Printf("F node = %+v", table)
 
-	result = append(result, iso3166{CountryName: "Germany", InternetccTLD: "de"})
+	var tbody *html.Node
+	for c := table.FirstChild; c != nil; c = c.NextSibling {
+		if c.Data == "tbody" {
+			log.Printf("0 %+v", c)
+			tbody = c
+		}
+	}
+	if tbody == nil || tbody.FirstChild == nil {
+		fatal(errors.New("tbody not found"))
+	}
 
+	for d := tbody.FirstChild; d != nil; d = d.NextSibling {
+		if d.Data == "tr" {
+			r := readrow(d)
+			if r != nil {
+				result = append(result, *r)
+			}
+		}
+	}
+
+	return result[1:]
+}
+
+func readrow(tablerow *html.Node) *iso3166 {
+	// log.Printf("table row %+v", tablerow)
+	result := &iso3166{}
+	tdCounter := 0
+	for td := tablerow.FirstChild; td != nil; td = td.NextSibling {
+		if td.Type != html.ElementNode {
+			continue
+		}
+
+		if tdCounter == 1 {
+			result.CountryName = renderNode(td.FirstChild)
+			if td.FirstChild.FirstChild != nil {
+				result.CountryName = renderNode(td.FirstChild.FirstChild)
+			}
+		}
+
+		if tdCounter == 7 {
+			result.InternetccTLD = renderNode(td.FirstChild.FirstChild)
+			if td.FirstChild.FirstChild.FirstChild != nil {
+				result.InternetccTLD = renderNode(td.FirstChild.FirstChild.FirstChild)
+			}
+		}
+
+		tdCounter++
+	}
+	// return &iso3166{CountryName: "Italy", InternetccTLD: "it"}
+	if tdCounter != 8 {
+		return nil
+	}
 	return result
 }
 
@@ -124,4 +180,14 @@ func renderNode(n *html.Node) string {
 		return ""
 	}
 	return buf.String()
+}
+func getElementAttributeValue(attrs []html.Attribute, elementName string) string {
+	result := ""
+	for _, v := range attrs {
+		if v.Key == elementName {
+			return v.Val
+		}
+	}
+	// fixme silent failure :/
+	return result
 }
