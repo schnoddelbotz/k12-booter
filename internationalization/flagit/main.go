3de2864dc6d16559ced28e2e26a8f62d21f0a72f
schnoddelbotz <jan@hacker.ch>
2023-04-15 16:32:22 +0200

    Drop the evil globals & KISS - drop regex #OverEngineering #OverKill

--- internationalization/flagit/main.go
+++ internationalization/flagit/main.go
@@ -4,37 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"schnoddelbotz/k12-booter/internationalization"
 )
 
-var flagKey = 0
-var flags []string
-var numFlags int
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	re := regexp.MustCompile(`[^\s]`)
-	flags = flagsAsArray()
-	numFlags = len(flags)
+	flags := flagsAsArray()
+	numFlags := len(flags)
+	flagKey := 0
 	for scanner.Scan() {
 		in := scanner.Text()
-		in = re.ReplaceAllStringFunc(in, getFlag)
-		fmt.Println(in)
+		for _, c := range in {
+			if c == ' ' {
+				fmt.Printf(" ")
+			} else {
+				flag := flags[flagKey]
+				flagKey++
+				if flagKey == numFlags {
+					flagKey = 0
+				}
+				fmt.Printf("%s", flag)
+			}
+		}
+		fmt.Println()
 	}
 
 	if scanner.Err() != nil {
 		panic(scanner.Err().Error())
 	}
-}
-
-func getFlag(in string) string {
-	flag := flags[flagKey]
-	flagKey++
-	if flagKey == numFlags {
-		flagKey = 0
-	}
-	return flag
 }
 
 func flagsAsArray() (flags []string) {
