666af070358cd8ca632c69dc3fe8b62c1b3d7eb8
schnoddelbotz <jan@hacker.ch>
2023-05-24 23:40:19 +0200

    make aptbuddy use the user's search query

--- cmd/aptbuddy/cmd/search.go
+++ cmd/aptbuddy/cmd/search.go
@@ -1,24 +1,30 @@
 package cmd
 
 import (
+	"fmt"
 	"schnoddelbotz/k12-booter/aptbuddy"
 	"schnoddelbotz/k12-booter/utility"
 
 	"github.com/spf13/cobra"
 )
 
+var numResults int
+
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
-	Use:   "search",
+	Use:   "search [flags] QUERY",
 	Short: "INCOMPLETE search on apt bleve index. Use bleve CLI...",
 	Long:  ``,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		b, err := aptbuddy.Open(language)
 		utility.Fatal(err)
-		b.Experiments()
+		r := b.Search(args[0], numResults, aptbuddy.HighlightDescAndTags)
+		fmt.Printf("%+v\n", r)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
+	searchCmd.Flags().IntVar(&numResults, "numResults", 10, "number of hits to print")
 }
